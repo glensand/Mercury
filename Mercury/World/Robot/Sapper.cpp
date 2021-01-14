9778ef42825e18ff21a0390058dc612ca2dd2aa7
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-01-14 11:32:35 +0700

    Update Sapper.cpp

--- Mercury/World/Robot/Sapper.cpp
+++ Mercury/World/Robot/Sapper.cpp
@@ -8,6 +8,15 @@ namespace merc
 Sapper::Sapper(IWorld& world, Terrain& exploredTerrain)
     : Robot(world, exploredTerrain)
 {
+        	size_t sapper_x, sapper_y;
+	while (true) {
+		sapper_x = rand() % (m_exploredTerrain.GetMap()).size();
+		sapper_y = rand() % (m_exploredTerrain.GetMap()[0]).size();
+		if (( (m_exploredTerrain.GetMap())[sapper_x][sapper_y] != CellType:: Unknown) && (( (m_exploredTerrain.GetMap())[sapper_x][sapper_y] != CellType::Rock) 
+			&& (sapper_x != coll_x || sapper_y != coll_y)) {
+			break;
+		}
+	}
 
 }
 
