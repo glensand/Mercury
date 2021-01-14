2ad25f0ada0417bccd17e89647dd0c1f0ebb2018
mmalakhova <71089865+mmalakhova@users.noreply.github.com>
2021-01-14 11:29:02 +0700

    Update Collector.cpp

--- Mercury/World/Robot/Collector.cpp
+++ Mercury/World/Robot/Collector.cpp
@@ -8,6 +8,18 @@ namespace merc
 Collector::Collector(IWorld& world, Terrain& terrain)
     : Robot(world, terrain)
 {
+        	size_t length = m_world.GetSizeX;
+	size_t width = m_world.GetSizeY;
+	while (true) {
+		size_t x = (rand() % (width - 1)) + 1;
+		size_t y = (rand() % (length - 1)) + 1;
+		Cell b = m_world.GetCell(x, y);
+		if (b.GetType != CellType::Bomb && b.GetType != CellType::Rock) {
+			m_x = x;
+			m_y = y;
+			break;
+		}
+	}
 
 }
 
@@ -19,6 +31,10 @@ void Collector::Collect()
 void Collector::Scan()
 {
     // TODO: update terrain
+        (m_exploredTerrain.GetCell(m_x - 1, m_y)).SetType((m_world.GetCell(m_x - 1, m_y)).GetType());
+    (m_exploredTerrain.GetCell(m_x, m_y + 1)).SetType((m_world.GetCell(m_x, m_y + 1)).GetType());
+    (m_exploredTerrain.GetCell(m_x + 1, m_y)).SetType((m_world.GetCell(m_x + 1, m_y)).GetType());
+    (m_exploredTerrain.GetCell(m_x , m_y + 1)).SetType((m_world.GetCell(m_x , m_y + 1)).GetType());
 }
 
 }
